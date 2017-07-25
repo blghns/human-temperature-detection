37b165656a021f66fe9d459a07137aac54aa3523
blghns <bilgehan.saglik@gmail.com>
2017-07-25 14:40:04 -0400

    Modified BernoulliRBM normalization function, normalizing input data as well. Added .idea folder back.

--- neuralSearch.py
+++ neuralSearch.py
@@ -14,16 +14,16 @@ def getMostSimilar(tarr):
     global logistic
     global rbm
     data = importDataset.dataParser()
+    normalizer = lambda x: (x + 20) / 1600
     if not classifierConstructed:
         tempArrs = [element["tempArr"] for element in data]
         npArr = np.array(tempArrs, 'float32')
-        divider = lambda x: x / (255 * 5)
-        adjustedArr = np.array([[divider(cell) for cell in element] for element in npArr])
-        rbm = BernoulliRBM(n_components=100, random_state=0, batch_size=100, learning_rate=0.006, n_iter=10, verbose=True)
+        adjustedArr = np.array([[normalizer(cell) for cell in element] for element in npArr])
+        rbm = BernoulliRBM(n_components=100, random_state=0, batch_size=5, learning_rate=0.06, n_iter=10, verbose=True)
         logistic = linear_model.LogisticRegression()
         logistic.C = 1.0
         classifier = Pipeline(steps=[('rbm', rbm), ('logistic', logistic)])
         classifier.fit(adjustedArr, np.array([(1 if 'Human' in item["name"] else 0) for item in data], 'float32'))
         classifierConstructed = True
-    prediction = classifier.predict([tarr])
-    return "Human" if prediction == 1 else "Not Human"
+    prediction = classifier.predict(np.array([normalizer(cell) for cell in tarr], 'float32'))
+    return "Human" if prediction[0] > 0 else "Not Human"
